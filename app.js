0b3f49cf247f30a32075ec5e45215d187048f592
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-07-09 14:41:48 +0200

    open ai api intergrated into the bot

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const { prefix, token } = require("./config.json");
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+    organization: "org-GBQZyg5M6nypkyHIOlgQegcQ",
+    apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
 
+const { prefix, token, botid } = require("./config.json");
+const { personality, cores } = require("./personality.json");
+const { bypass, banned} = require("./users.json");
+const maxInput = 150;
+const memory = [];
+const users = {};
+const timeout = 30;
+const maxUsagePerXXMinutes = 5;
+const Minutes = 60;
 const { Client, Intents, Collection } = require('discord.js');
 const bot = new Client({ 
     intents: [
@@ -8,7 +22,20 @@ const bot = new Client({
         Intents.FLAGS.GUILD_MEMBERS
     ] 
 });
-
+setInterval(() => {
+    for (const [key, value] of Object.entries(users)) {
+        if (value > 0) {
+            users[key]--;
+        }
+    }
+}, 1000 * 60);
+setInterval(() => {
+    for (const [key, value] of Object.entries(users)) {
+        if (value < 0) {
+            users[key] = 0;
+        }
+    }
+}, 1000 * 60 * Minutes);
 const fs = require("fs");
 
 bot.commands = new Collection();
@@ -57,7 +84,7 @@ bot.on("messageCreate", async message => {
     //Check for prefix
     if(!cmd.startsWith(prefix)) {
         //execute code here if pinged
-        if(message.mentions.has(bot.user)) {
+        if(message.mentions.has(bot.user) && message.channel.name !== "legacy-plus-bot") {
             message.channel.send("mfw");
             return;
         }
@@ -85,6 +112,43 @@ bot.on("messageCreate", async message => {
             
             return;
         }
+        else if (message.channel.name === "legacy-plus-bot" && message.mentions.users.first()) {
+            if (message.mentions.users.first().id !== botid) return; // only respond to pings of this bot
+            if (banned.includes(message.author.id)) return; // don't respond to banned users
+            if (users[message.author.id] == null) users[message.author.id] = 0;
+            if (message.content.length > maxInput) {
+                message.channel.send("I'm sorry <@" + message.author.id + ">, Your message is too long.");
+                return;
+            }
+            if (users[message.author.id] > 0) {
+                message.channel.send("I'm sorry <@" + message.author.id + ">, You have to wait " + users[message.author.id] + " minut" + (users[message.author.id] != 1 ? "es" : "e") + " before you can talk to me again.");
+                return;
+            }
+            if (users[message.author.id]-- <= -maxUsagePerXXMinutes && !bypass.includes(message.author.id)) users[message.author.id] = timeout;
+
+            let promptByUser = message.content;
+            //filter out any pings
+            promptByUser = promptByUser.replace(/<(?:@[!&]?|#)\d+>/g, "");
+            memory.push({role: "user", content: promptByUser});
+
+
+            const chatCompletion = openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                max_tokens: 50,
+                messages: getMemory()
+            });
+            chatCompletion.then(
+                (response) => {
+                    message.channel.send(response.data.choices[0].message.content);
+                    memory.push({role: "assistant", content: response.data.choices[0].message.content});
+                }
+            ).catch(
+                (err) => {
+                    message.channel.send("Error.");
+                    memory.splice(0,memory.length);
+                }
+            );
+        }
         return;
     } 
 
@@ -93,6 +157,20 @@ bot.on("messageCreate", async message => {
     if(commandfile) commandfile.run(bot,message,args);
 
 });
-
+function getMemory (memorystrength = 6) {
+    const response = [];
+    const days = new Date().getFullYear() % 4 == 0 ? 366 : 365;
+    response.push({role: "system", content: personality + getRandomCore(days)});
+    for (let i = Math.max(0, memory.length - memorystrength); i < memory.length; i++) {
+        response.push(memory[i]);
+    }
+    return response;
+}
+function getRandomCore (seed = -1) {
+    if (seed == -1) seed = (Math.random()*366)|0;
+    rand = Math.sin(seed) * 10001 - Math.floor(Math.sin(seed) * 10001);
+    console.log(rand * cores.length | 0);
+    return cores[rand * cores.length | 0];
+}
 //Token needed in config.json
 bot.login(token);
